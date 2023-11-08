f3c23f34c2d567f689c22f59a4643fdf420496af
Camilo Romero <romerocamilo24@gmail.com>
2023-11-08 12:36:46 -0500

    public strings

--- Models/Index.cshtml.cs
+++ Models/Index.cshtml.cs
@@ -6,6 +6,10 @@ namespace portfolio.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    
+    // public string variable. get is used to allow retrieving the message outside of the class
+    // private set means that we can only modify this string message within this class. 
+    public string Message { get; private set; } = "";
 
     public IndexModel(ILogger<IndexModel> logger)
     {
@@ -14,6 +18,7 @@ public class IndexModel : PageModel
 
     public void OnGet()
     {
-
+        // once we are loaded into the biography page. 
+        Message += $" Server time is { DateTime.Now }";
     }
 }
--- Models/biography.cshtml.cs
+++ Models/biography.cshtml.cs
@@ -7,7 +7,7 @@ public class biographyModel : PageModel
 {
     private readonly ILogger<biographyModel> _logger;
 
-    public string Message { get; private set; } = "PageModel in C#";
+    public string Message { get; private set; } = "";
 
     public biographyModel(ILogger<biographyModel> logger)
     {
