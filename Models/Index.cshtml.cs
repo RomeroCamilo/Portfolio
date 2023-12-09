9aa58e26b320588ae4ae2f7a8e087b0795b28c62
Camilo Romero <romerocamilo24@gmail.com>
2023-12-08 20:02:15 -0500

    updates

--- Models/Index.cshtml.cs
+++ Models/Index.cshtml.cs
@@ -14,6 +14,7 @@ namespace portfolio.Pages
         private readonly ILogger<IndexModel> _logger;
         private readonly IHttpClientFactory _httpClientFactory;
 
+        // message variable that can only be modified within this function but can be get from outside.
         public string Message { get; private set; } = "";
 
         // will store the response of the azure function api
@@ -36,17 +37,11 @@ namespace portfolio.Pages
 
             /* if sucessful response code such as 200, get the response */
             if (response.IsSuccessStatusCode)
-            {
                 FunctionResponse = await response.Content.ReadAsStringAsync();
-            }
-            /* if connection failed ( non 200 code ) */
+            /* if connection failed ( non-200 code ) */
             else
-            {
                 FunctionResponse = "Error: Unable to fetch data from the Azure Function.";
-            }
-
-            // Once the response is fetched, you can perform additional actions or display it as needed.
-            
+        
             // Example: Appending the current server time to the Message
             Message += $"Server time is {DateTime.Now}";
         }
