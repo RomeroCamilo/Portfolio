8879531739687dea3c579ee32de92bd7a9d0e0a1
Camilo Romero <romerocamilo24@gmail.com>
2023-12-10 18:15:20 -0500

    cleaned

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -28,22 +28,25 @@ namespace portfolio.Pages
 
         public async Task OnGetAsync()
         {
+            
             // Make an HTTP request to the Azure Function here
             var client = _httpClientFactory.CreateClient();
             var functionUrl = "https://pythonapiservices.azurewebsites.net/api/read";
 
             // making an api call and storing the response code.
-            HttpResponseMessage response = await client.GetAsync(functionUrl);
+            //HttpResponseMessage response = await client.GetAsync(functionUrl);
 
             /* if sucessful response code such as 200, get the response */
-            if (response.IsSuccessStatusCode)
-                FunctionResponse = await response.Content.ReadAsStringAsync();
+            //if (response.IsSuccessStatusCode)
+                //FunctionResponse = await response.Content.ReadAsStringAsync();
             /* if connection failed ( non-200 code ) */
-            else
-                FunctionResponse = "Error: Unable to fetch data from the Azure Function.";
-        
+           // else
+                //FunctionResponse = "Error: Unable to fetch data from the Azure Function.";
+
+            
             // Example: Appending the current server time to the Message
             Message += $"Server time is {DateTime.Now}";
+
         }
     }
 }
