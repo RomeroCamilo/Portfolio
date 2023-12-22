f21bf17257e1cce1664df7f10d837a3e8bdb2938
Camilo Romero <romerocamilo24@gmail.com>
2023-12-22 16:25:34 -0500

    game pic added

---
+++ Pages/Projects.cshtml.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+// razor page index page.
+
+namespace portfolio.Pages
+{
+    public class ProjectsModel : PageModel
+    {
+        private readonly ILogger<ProjectsModel> _logger;
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        // message variable that can only be modified within this function but can be get from outside.
+        public string Message { get; private set; } = "";
+
+        // will store the response of the azure function api
+        public string FunctionResponse { get; set; }
+
+        public ProjectsModel(ILogger<ProjectsModel> logger, IHttpClientFactory httpClientFactory)
+        {
+            _logger = logger;
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task OnGetAsync()
+        {
+            
+            // Make an HTTP request to the Azure Function here
+            var client = _httpClientFactory.CreateClient();
+            var functionUrl = "https://pythonapiservices.azurewebsites.net/api/read";
+
+            // making an api call and storing the response code.
+            //HttpResponseMessage response = await client.GetAsync(functionUrl);
+
+            /* if sucessful response code such as 200, get the response */
+            //if (response.IsSuccessStatusCode)
+                //FunctionResponse = await response.Content.ReadAsStringAsync();
+            /* if connection failed ( non-200 code ) */
+           // else
+                //FunctionResponse = "Error: Unable to fetch data from the Azure Function.";
+
+            
+            // Example: Appending the current server time to the Message
+            Message += $"Server time is {DateTime.Now}";
+
+        }
+    }
+}
